34dd0a614c5bc7f144fe047ba5ea5df6e7b270c3
yikuansun <yikuansun2015@gmail.com>
2021-12-11 12:12:57 -0500

    Titlebar

--- window/renderscript.js
+++ window/renderscript.js
@@ -1,5 +1,11 @@
 const { remote } = require("electron");
 const { getGlobal } = remote;
+const customTitlebar = require("custom-electron-titlebar");
+
+new customTitlebar.Titlebar({
+	backgroundColor: customTitlebar.Color.fromHex("#353535"),
+    menu: null
+});
 
 options = getGlobal("options");
 
@@ -10,7 +16,7 @@ else {
     }
 }
 
-Photopea.initEmbed(document.querySelector("div"), JSON.stringify(options)).then(function(frame) {
+Photopea.initEmbed(document.querySelector("#outerWrap"), JSON.stringify(options)).then(function(frame) {
     document.querySelector("#loadingscreen").remove();
     for (var resource of getGlobal("resources")) Photopea.addBinaryAsset(frame.contentWindow, resource).then();
     if (getGlobal("openedFile")) Photopea.addBinaryAsset(frame.contentWindow, getGlobal("openedFile")).then();
