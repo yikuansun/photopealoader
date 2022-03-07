0443e4ca68247b7194eebb2e93ebe43852c46d3d
yikuansun <yikuansun2015@gmail.com>
2022-03-06 21:12:08 -0500

    Add helpful status messages

--- window/renderscript.js
+++ window/renderscript.js
@@ -1,7 +1,10 @@
 window.ppapi.getGlobal('resources', 'openedFile', 'openedFilePath', 'options').then(function (vars) {
     let { resources, openedFile, openedFilePath, options } = vars;
+    document.querySelector("#statusTag").innerText = "Initiating embed...";
     Photopea.initEmbed(document.querySelector("#outerWrap"), options).then(async function (frame) {
+        document.querySelector("#statusTag").innerText = "Loading resources...";
         for (var resource of resources) await Photopea.addBinaryAsset(resource);
+        document.querySelector("#statusTag").innerText = "Opening...";
         if (openedFile) await Photopea.addImage(openedFile, openedFilePath);
         document.querySelector("#loadingscreen").remove();
         // tests
